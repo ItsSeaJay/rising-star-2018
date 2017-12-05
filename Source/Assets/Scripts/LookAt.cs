f603ab4f499c66c6bd927a3bc7bfaaadd0b9564f
Unknown <b6010572@my.shu.ac.uk>
2017-12-05 21:19:10 +0000

    Added an option to smooth the rotation of the LookAt script

--- Source/Assets/Scripts/LookAt.cs
+++ Source/Assets/Scripts/LookAt.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class LookAt : MonoBehaviour
 {
     [SerializeField]
+    private bool smooth = true;
+    [SerializeField]
+    private float speed = 5.0f;
+    [SerializeField]
     private Transform target;
 
     void Update ()
     {
-        transform.LookAt(target);
+        if (smooth)
+        {
+            // Smoothly rotate towards the target point
+            Vector3 positionDifference = target.position - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(positionDifference);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.LookAt(target);
+        }
     }
 }
