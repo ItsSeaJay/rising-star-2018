a5ea81d5294c496c9739085634cbdf660fb5b19f
Unknown <b6010572@my.shu.ac.uk>
2018-01-13 20:41:02 +0000

    Ocean now scrolls infinitely

---
+++ Source/Assets/Scripts/OffsetScroller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This the following script was adapted from the Unity tutorial:
+// 2D Scrolling Background
+// https://unity3d.com/learn/tutorials/topics/2d-game-creation/2d-scrolling-backgrounds
+
+public class OffsetScroller : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 velocity;
+
+    private Vector2 offset;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        offset = rend.sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    void Update()
+    {
+        offset.x += velocity.x * Time.deltaTime;
+        offset.y += velocity.x * Time.deltaTime;
+
+        rend.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+
+    void OnDisable()
+    {
+        rend.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
