8b615bacba086d37513878b2d224a6232917eb42
Unknown <b6010572@my.shu.ac.uk>
2018-02-03 19:19:27 +0000

    Made the ship move with player axes

--- Source/Assets/Own Assets/Scripts/ControlledRotator.cs
+++ Source/Assets/Own Assets/Scripts/ControlledRotator.cs
@@ -2,15 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlledRotator : MonoBehaviour {
+public class ControlledRotator : MonoBehaviour
+{
+    [SerializeField]
+    private string axis = "Horizontal";
+    [SerializeField]
+    private Vector3 amount;
+    [SerializeField]
+    private bool rotateX, rotateY, rotateZ;
+    [SerializeField]
+    private bool invertX, invertY, invertZ;
+    [SerializeField]
+    private bool smooth = false;
 
-	// Use this for initialization
-	void Start () {
-		
+    private Vector3 startingAngle;
+    
+	void Start ()
+    {
+        startingAngle = transform.localEulerAngles;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (!smooth)
+        {
+            RotateDirect();
+        }
 	}
+
+    void RotateDirect()
+    {
+        float input = Input.GetAxis(axis);
+        Vector3 currentAngle = startingAngle;
+
+        if (rotateX && !invertX)
+        {
+            currentAngle.x = input * amount.x;
+        }
+        else if (rotateX && invertX)
+        {
+            currentAngle.x = -input * amount.x;
+        }
+
+        if (rotateY && !invertY)
+        {
+            currentAngle.y = input * amount.y;
+        }
+        else if (rotateY && invertY)
+        {
+            currentAngle.y = -input * amount.y;
+        }
+
+        if (rotateZ && !invertZ)
+        {
+            currentAngle.z = input * amount.z;
+        }
+        else if (rotateZ && invertZ)
+        {
+            currentAngle.z = -input * amount.z;
+        }
+
+        transform.localEulerAngles = currentAngle;
+    }
 }
